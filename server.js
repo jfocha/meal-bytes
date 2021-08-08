505d24bd802ff1ec4f0e5934aef87e5d1d72a7c2
Shahab Geravesh <59784276+Shahabgeravesh@users.noreply.github.com>
2021-08-07 18:19:17 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//path
+
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-// add stylesheets
-app.use(express.static(path.join(__dirname, 'public')));
-
-// turn on routes
-app.use(routes);
-
-// turn on connection to db and server
+//require the models folder because there is an index file in the folder that exports all files within it
+//             
+app.use(require('./models'));
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+    app.listen(PORT, () => console.log('Now listening'));
+  });
