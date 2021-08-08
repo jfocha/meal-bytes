5714e4bb304855b5992456d5f635ee4046f5df6a
Shahab Geravesh <59784276+Shahabgeravesh@users.noreply.github.com>
2021-08-07 18:46:24 -0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize("recipe_db", "root", "Shahab1373!!", {
+  : new Sequelize("recipe_db", "root", "password", {
       host: "localhost",
       dialect: "mysql",
       port: 3306,
