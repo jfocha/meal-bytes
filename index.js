0bceeefbbf6d3734c1ec6d93fd67ffa4ade55d6a
Shahab Geravesh <59784276+Shahabgeravesh@users.noreply.github.com>
2021-08-07 17:04:42 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const recipe = require('./recipe');
+
+module.exports = recipe;
+
+//index.js file will export any models found within the models folder so that 
+//they can be referenced from other files by just calling on the models folder
