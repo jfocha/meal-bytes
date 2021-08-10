9a54f9ccb72597407acc5cd95e434e57a6ef894e
jfocha <jfocha@gmail.com>
2021-08-09 20:51:28 -0700

    hitting routes

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,15 +1,15 @@
-require('dotenv').config()
+// require('dotenv').config()
 
-const api_key = process.env.API_KEY;
+// const api_key = process.env.API_KEY;
 
-fetch(`https://api.edamam.com/${api_key}/recipes/v2`)
-    .then(response => {
-        return response.json();
-    }).then(data => {
-        console.log(data);
-        // use the data how ever you want
+// fetch(`https://api.edamam.com/${api_key}/recipes/v2`)
+//     .then(response => {
+//         return response.json();
+//     }).then(data => {
+//         console.log(data);
+//         // use the data how ever you want
 
-    })
-    .catch(err => {
-        console.error(err);
-    });
+//     })
+//     .catch(err => {
+//         console.error(err);
+//     });
