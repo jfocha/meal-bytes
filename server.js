90d39383de0aa735feb4b149ab091bf07e59069a
jfocha <jfocha@gmail.com>
2021-08-09 22:29:09 -0700

    server working. Please install new dependencies

--- server.js
+++ server.js
@@ -1,26 +1,52 @@
 const path = require('path');
 const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+const session = require('express-session');
+require('dotenv').config();
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//path
 
-const sequelize = require('./config/connection');
-
-const hbs = exphbs.create({});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-// app.use(express.static(path.join(__dirname, 'public')));
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-//require the models folder because there is an index file in the folder that exports all files within it
-//             
-app.use(require('./models'));
+const sess = {
+  secret: process.env.SECRET, // Please update your .env with the SECRET. Also, don't forget to update Heroku settings!
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+// add stylesheets
+app.use(express.static(path.join(__dirname, 'public')));
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-  });
+  app.listen(PORT, () => console.log('Now listening'));
+});
+
+
+// require('../../models'); belongs in the api routes
+
+// //require the models folder because there is an index file in the folder that exports all files within it
+// //             
+// app.use(require('./models'));
