2b91bce67857753b7e37e68feed9d1bab07ad80b
Shahab Geravesh <59784276+Shahabgeravesh@users.noreply.github.com>
2021-08-10 20:46:28 -0700

    Add files via upload

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const Recipe = require('./Recipe');
 const User = require('./User');
-const favorite=require('./favorite');
+const favorite = require('./favorite');
+const Ingredients = require('./Ingredients');
 
 //need to create associations and register foreign keys
 User.hasMany(Recipe, {
@@ -11,7 +12,19 @@ Recipe.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
-module.exports = { User, Recipe, favorite };
+Ingredients.belongsToMany(Recipe, {
+    through: "Recipe_Ingredients",
+    as: "Ingredients",
+    foreignKey: "Ingredients_id",
+});
+
+Recipe.belongsToMany(Ingredients, {
+    through: "Recipe_Ingredients",
+    as: "Recipe",
+    foreignKey: "Recipe_id",
+});
+
+module.exports = { User, Recipe, favorite, Ingredients };
 
 //index.js file will export any models found within the models folder so that 
 //they can be referenced from other files by just calling on the models folders
