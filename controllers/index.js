1583d47b32c9f7ac61efbc0c52b185c3a6b76820
jfocha <jfocha@gmail.com>
2021-08-15 15:02:38 -0700

    started dashboard route

--- controllers/index.js
+++ controllers/index.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
 const apiRoutes = require('./api');
 const recipes = require('./recipes-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/recipes', recipes);
+router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
